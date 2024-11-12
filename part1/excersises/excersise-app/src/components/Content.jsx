c0eefabf2b44cc258b2b3f20d5c7a5b2b5d80233
Pawel <pawel.werbowy@gmail.com>
2024-11-12 12:10:46 +0000

    Update Content.jsx

--- part1/excersises/excersise-app/src/components/Content.jsx
+++ part1/excersises/excersise-app/src/components/Content.jsx
@@ -4,15 +4,15 @@ const Content = ({
   part1,
   part2,
   part3,
-  excercises1,
-  excercises2,
-  excercises3,
+  exercises1,
+  exercises2,
+  exercises3,
 }) => {
   return (
     <div>
-      <Part part={part1} excercises={excercises1} />
-      <Part part={part2} excercises={excercises2} />
-      <Part part={part3} excercises={excercises3} />
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </div>
   );
 };
