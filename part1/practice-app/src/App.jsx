92d525adb7bcf53ea558a4bbc87d80e910d09c8c
Pawel <pawel.werbowy@gmail.com>
2024-11-20 21:50:43 +0000

    new practice part1/ d

--- part1/practice-app/src/App.jsx
+++ part1/practice-app/src/App.jsx
@@ -1,24 +1,37 @@
-const Hello = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  );
-};
+import { useState } from "react";
+import Display from "./components/Display";
+import Button from "./components/Button";
 
 const App = () => {
-  const name = "Peter";
-  const age = 10;
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const [total, setTotal] = useState(0);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
 
   return (
     <div>
-      <h1>Greetings</h1>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(" ")}</p>
 
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <p>total {total}</p>
     </div>
   );
 };
