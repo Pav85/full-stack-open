ed5c1bd80595bae78bf4adf73e587fe5b2ba03b1
Pawel <pawel.werbowy@gmail.com>
2024-11-21 10:32:25 +0000

    startup files ready

--- part1/exercises/exercise-app-6-14/src/App.jsx
+++ part1/exercises/exercise-app-6-14/src/App.jsx
@@ -1,33 +1,12 @@
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
+import { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+  return <div>code here</div>;
 };
 
 export default App;
