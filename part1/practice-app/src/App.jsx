aa048b2b51011c43d8e838644073884766722392
Pawel <pawel.werbowy@gmail.com>
2024-11-21 09:45:51 +0000

    Update App.jsx

--- part1/practice-app/src/App.jsx
+++ part1/practice-app/src/App.jsx
@@ -1,37 +1,18 @@
 import { useState } from "react";
-import Display from "./components/Display";
-import Button from "./components/Button";
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
+  const [value, setValue] = useState(10);
 
-  const [total, setTotal] = useState(0);
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    const updatedLeft = left + 1;
-    setLeft(updatedLeft);
-    setTotal(updatedLeft + right);
+  const setToValue = (newValue) => () => {
+    console.log("value now", newValue); // print the new value to console
+    setValue(newValue);
   };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-    setTotal(left + updatedRight);
-  };
-
   return (
     <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(" ")}</p>
-
-      <p>total {total}</p>
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   );
 };
