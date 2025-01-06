cf73a0aae778357a4cf6d3b0a8315f58a0f260b0
Pawel <pawel.werbowy@gmail.com>
2025-01-06 14:36:00 +0000

    exercise 2.11

--- part2/exercises/phonebook-app-6-10/src/App.jsx
+++ part2/exercises/phonebook-app-6-10/src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
   const [filteredPersons, setFilteredPersons] = useState(persons);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+      setFilteredPersons(response.data);
+    });
+  }, []);
 
   const capitalizeFirstLetter = (name) => {
     return name
