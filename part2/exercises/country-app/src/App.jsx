557e840ccb8a0a56127e38017caa5f399d36757b
Pawel <pawel.werbowy@gmail.com>
2025-01-15 19:08:14 +0000

    Update App.jsx

--- part2/exercises/country-app/src/App.jsx
+++ part2/exercises/country-app/src/App.jsx
@@ -1,10 +1,12 @@
 import Search from "./components/Search";
+import Message from "./components/Message";
 
 const App = () => {
   return (
     <div>
       <h1>Country App</h1>
       <Search />
+      <Message />
     </div>
   );
 };
