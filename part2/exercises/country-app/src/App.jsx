dff08beea0b97e94e9dc1801ed0e8bbf8eaa10d1
Pawel <pawel.werbowy@gmail.com>
2025-01-16 10:12:50 +0000

    Update App.jsx

--- part2/exercises/country-app/src/App.jsx
+++ part2/exercises/country-app/src/App.jsx
@@ -5,10 +5,14 @@ import Message from "./components/Message";
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [searchCountry, setSearchCountry] = useState("");
+  const [message, setMessage] = useState(null);
 
   const handleSearchCountry = (event) => {
     console.log(event.target.value);
+    const searchValue = event.target.value;
+    setSearchCountry(searchValue);
   };
+
   return (
     <div>
       <h1>Country App</h1>
