84bccf357a8208cdcb18f956eba19e00bd5c62cf
Pawel <pawel.werbowy@gmail.com>
2025-07-02 19:27:59 +0100

    Add dotenv support and example env file
    Introduced dotenv for environment variable management and added a .env.example file to document required environment variables. Updated .gitignore to exclude .env files. Refactored index.js to load environment variables and moved the Note model to a separate file.

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,30 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const Note = require("./models/note");
 
 const app = express();
-
-const password = process.argv[2];
-const url = `mongodb+srv://fullstack:${password}@cluster0.85fctfq.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
 const PORT = process.env.PORT || 3001;
-
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Note = mongoose.model("Note", noteSchema);
 
 app.use(express.json());
 app.use(cors());
